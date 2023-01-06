644f5dcc00d16bb21fc6e0c3f02b46af2a8fed79
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2023-01-06 21:11:22 +0100

    copied mosh code .

--- tags/models.py
+++ tags/models.py
@@ -2,21 +2,13 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
 
 class Tag(models.Model):
-    labels = models.CharField(max_length=255)
+    label = models.CharField(max_length=255)
 
 
-
-class TaggedItem():
-#what tag applied to what item
-    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
-
-    #to define a generic relationship we need to define tree fields which are content_type, object_id and content_object
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-
-
-
