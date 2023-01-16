dec27f27c919b542b6b1eb90e601465cc6693497
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2023-01-16 12:46:16 +0100

    added pagination, sorting, filtering and ordering.

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet, basename='products')
+router.register('products', views.ProductViewSet,
+basename='products'
+ )
 router.register('collections', views.CollectionViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup ='product')
--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from rest_framework.pagination import PageNumberPagination
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sessions',
     'rest_framework',
+    'django_filters', 
     'playground',
     'debug_toolbar',
     'store',
@@ -142,5 +144,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING':False
+    'COERCE_DECIMAL_TO_STRING':False,
+#    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE':10,
 }
