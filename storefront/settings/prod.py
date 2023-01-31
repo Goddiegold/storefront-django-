b2b684d818deda582beeac8bcafeac95d47462d5
Godwin <gehikhamhen247@gmail.com>
2023-01-31 09:47:53 +0100

    added prod url to allowed host.

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -2,8 +2,8 @@ import os
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://storefront-django.onrender.com"]
